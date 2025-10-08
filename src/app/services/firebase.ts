114fb8430b8f639e0b78037d327121998b92ed59
capstone805d-gif <tu_email_de_capstone805d-gif@example.com>
2025-10-07 23:06:44 -0300

    Subo mis archivos

---
+++ src/app/services/firebase.ts
@@ -0,0 +1,22 @@
+import { inject, Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { User } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Firebase {
+  
+  auth = inject(AngularFireAuth);
+
+
+// Auntenticacion
+
+// Acceder
+
+  signIn(user:User){
+    return signInWithEmailAndPassword(getAuth(), user.email, user.password)
+  }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+
+    firebaseConfig: {
+    apiKey: "AIzaSyCOW2Q-5T-bnOApwAEnuxLiydmu1epxcMY",
+    authDomain: "avicolapp.firebaseapp.com",
+    projectId: "avicolapp",
+    storageBucket: "avicolapp.firebasestorage.app",
+    messagingSenderId: "739613833371",
+    appId: "1:739613833371:web:b75b64ddf951701a5a2e3e",
+    measurementId: "G-ZT13YWYEC2"
+  }
+
+};
